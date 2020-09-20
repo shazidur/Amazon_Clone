a7acb02235e2bf67bb839f759a3da314587c4f55
shazidur <jadushazid007@ggmail.com>
2020-09-21 03:41:57 +0900

    cloud function done

---
+++ functions/index.js
@@ -0,0 +1,39 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const stripe = require("stripe")(
+  "sk_test_51HSzwrIyXWqKfeudBM7oxUnOsJVFT5PzlUrkyo9VIixrRqUFkVQJn650LrKAI2NDbPOnsGkpNP5Dh7Nr6srMRmDf00g1N04UgU"
+);
+
+//API
+
+//App Config
+const app = express();
+
+// Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// API routes
+app.get("/", (request, response) => response.status(200).send("Hello word"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log(" Payment Request Recived .. for amount >>", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// Listen commend
+exports.api = functions.https.onRequest(app);
+
+//example end-point
+//http://localhost:5001/mazon-9f693/us-central1/api
