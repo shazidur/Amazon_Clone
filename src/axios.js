b69fcb438d456905d7e3b4b9ea12808ba3411a1d
shazidur <jadushazid007@ggmail.com>
2020-09-21 03:47:24 +0900

    Front-end fix

---
+++ src/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5001/mazon-9f693/us-central1/api",
+});
+
+//
+
+export default instance;
+
+//  http://localhost:5001/mazon-9f693/us-central1/api
+//  http://localhost:5001/mazon-9f693/us-central1/api
